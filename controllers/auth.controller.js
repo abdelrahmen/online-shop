c2a709e5c1f27c87a1b5faa547f97e40f87b3135
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-02-11 00:00:25 +0200

    added validation before registration

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,6 @@
+const { validationResult } = require("express-validator");
 const authModel = require("../models/auth.model");
+const expressValidator = require("express-validator").validationResult;
 
 exports.getSignup = (req, res, next) => {
   res.render("signup");
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const bodyParser = require("body-parser");
+const check = require("express-validator").check;
 
 const authController = require("../controllers/auth.controller");
 
@@ -8,6 +9,18 @@ router.get("/signup", authController.getSignup);
 router.post(
   "/signup",
   bodyParser.urlencoded({ extended: true }),
+  check("username").not().isEmpty().withMessage("username is required"),
+  check("email")
+    .not()
+    .isEmpty()
+    .withMessage("email is required")
+    .isEmail()
+    .withMessage("invalid email format"),
+  check("password").isLength({ min: 6, max: 32 }).withMessage("password must be between 6 & 32 characters"),
+  check("confirmPassword").custom((value, { req }) => {
+    if ((value = req.body.password)) return true;
+    else throw "the 2 password fields must be equal";
+  }),
   authController.postSignup
 );
 
@@ -19,6 +32,6 @@ router.post(
   authController.postLogin
 );
 
-router.all('/logout', authController.logout)
+router.all("/logout", authController.logout);
 
 module.exports = router;
