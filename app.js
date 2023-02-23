bc6dbffb4ce6280412c9012b7ebbe15617c74fd0
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-02-23 23:56:33 +0200

    added a page for error 404

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.get("/error", (req, res, next) => {
   res.render("error.ejs", {
     isUser: req.session.userId,
     isAdmin: req.session.isAdmin,
+    pageTitle: "Error",
   });
 });
 
@@ -52,6 +53,16 @@ app.get("/not-admin", (req, res, next) => {
   res.render("not-admin.ejs", {
     isUser: req.session.userId,
     isAdmin: req.session.isAdmin,
+    pageTitle: "not-accessible",
+  });
+});
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.render("not-found", {
+    isUser: req.session.userId,
+    isAdmin: req.session.isAdmin,
+    pageTitle: "not-found",
   });
 });
 
